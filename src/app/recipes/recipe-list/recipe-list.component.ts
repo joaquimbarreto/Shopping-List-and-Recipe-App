ed1fb78501111cd6119e873762f26be727a73a0d
Joaquim Barreto <joaquim@barreto.co>
2020-01-10 16:25:03 +0100

    model recipe class

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      "Curry",
+      "Chicken curry",
+      "https://statics.cucchiaio.it/content/cucchiaio/it/ricette/2018/07/spezzatino-di-vitello-al-curry/jcr:content/header-par/image-single.img10.jpg/1530691026192.jpg"
+    )
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
